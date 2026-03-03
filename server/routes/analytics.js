0fb77530d046203bef02a93e92de449ba501ac22
guptaanand-source <guptaanand96287@gmail.com>
2026-03-03 11:15:20 +0530

    initial project upload
    initial project upload

--- server/routes/analytics.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router  = express.Router();
-const { getEcoAnalytics } = require('../controllers/analyticsController');
-const { verifyToken } = require('../middleware/auth');
-const { authorizeRoles } = require('../middleware/role');
-
-router.get('/eco', verifyToken, authorizeRoles('admin', 'teacher'), getEcoAnalytics);
-
-module.exports = router;
